8b00655275ceccc10d1c743a206972237cfbfb95
yayun0215 <zx125685>
2016-10-31 16:23:05 +0800

    ex

--- src/ex/ex05.java
+++ src/ex/ex05.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /*
  * Topic: 使用者輸入一個大正整數值，請設計一個函數fun1(n)可以計算該大數字裡的數字總合。例如:輸入: 4832 則輸出 17
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021062 鄭雅韵
  */
 
 public class ex05 {
