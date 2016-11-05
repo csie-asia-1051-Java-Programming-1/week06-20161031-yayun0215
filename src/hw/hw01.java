26a2b8b257dfd3e90d2027045eac8cf6d11b31c8
yayun0215 <105021062@live.asia.edu.tw>
2016-11-05 18:27:58 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,9 +1,25 @@
 package hw;
-
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println(fun(scn.nextInt(), scn.nextInt()));
+
+	}
+
+	public static int fun(int v1, int v2) {
+		int a=0;
+		switch (v2) {
+		case 1:
+			a = (v1 - 32) * (5 / 9);
+			break;
+		case 2:
+			a = (v1 * 9 / 5 + 32);
+			break;
+		}
+		return a;
 
 	}
 
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,28 @@
 package hw;
-
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.println(fun(scn.nextInt(),scn.nextInt()));
 
+	}
+
+	public static float fun(float nextInt, int nextInt2) {
+
+		float a=0;
+		switch(nextInt2){
+		case 1:
+			a = (float) ((nextInt - 170) * 0.6 + 62);
+			break;
+		case 2:
+			a = (float) ((nextInt - 158) * 0.5 + 52);
+			break;
+
+		}
+		
+		return a;
 	}
 
 }
